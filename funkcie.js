e1181faaf10da52554d2f49d6cc6420abec883a0
boomafromboomiland <lubomira.kovacova@avon.com>
2022-01-22 15:12:28 +0100

    1 file modified, 1 added

---
+++ funkcie.js
@@ -0,0 +1,17 @@
+function back5() {
+  return 5;
+}
+
+function helloWorld() {
+  console.log('Ahoj svet!');
+}
+
+/*let a = back5();
+console.log(a);*/
+
+function sum2numbers(first, second) {
+  return first + second;
+}
+
+let outcome = sum2numbers(10, 55);
+console.log(outcome);
--- ukoly/ukol5.js
+++ ukoly/ukol5.js
@@ -1,9 +1,21 @@
-let number = 1;
-set sum = 0;
+/*let number = Number(prompt('Write any number'));
+let sum = 0;
 
-while (sum !== 0) {
-  number = Number(prompt('Write any number'));
+while (number !== 0) {
   sum = sum + number;
-  console.log('Sum of your numbers is: ' + sum + '.')
-  }
+  number = Number(prompt('Write any number'));
+  console.log('Sum of your numbers is: ' + sum + '+' + number + '.');
+}
+*/
+
+for (let i = 12, j = 12 ** 3; i < 12 + j; i = i * 2) {
+  console.log(i);
+}
+
+let i = 12;
+let j = 12 ** 3;
+
+while (i < 12 + j) {
+  console.log(i);
+  i = i * 2;
 }
